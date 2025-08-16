3dc23e9db6ac827077316906d224807368d089b4
Momena <momena.akhtar19@gmail.com>
2025-08-16 19:16:31 +0500

    on click route to signin

--- frontend_web/app/components/ui/signup.tsx
+++ frontend_web/app/components/ui/signup.tsx
@@ -1,18 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function Signup() {
+    const router = useRouter();
+
+    const handleSignupClick = () => {
+        router.push('/signin');
+    };
+
     return (
-        <button className="font-semibold
-        cursor-pointer
-        px-5
-        py-2
-        bg-gradient-to-r
-        from-primary
-        to-accent
-        text-white
-        rounded-full 
-        transition-all
-        duration-300
-        text-base
-        hover:scale-102">
+        <button 
+            onClick={handleSignupClick}
+            className="font-semibold
+            cursor-pointer
+            px-5
+            py-2
+            bg-gradient-to-r
+            from-primary
+            to-accent
+            text-white
+            rounded-full 
+            transition-all
+            duration-300
+            text-base
+            hover:scale-102"
+        >
             Sign Up
         </button>
     );
