072a73406a7423356a96a5cbf1c84b3ce1688e48
Momena <momena.akhtar19@gmail.com>
2025-08-16 20:07:04 +0500

    wrap app in context provider

--- frontend_web/app/layout.tsx
+++ frontend_web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -24,7 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
